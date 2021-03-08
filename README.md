97699d4b60935999c66853c9ba20986c0e9f272f
Cody Johnson <codymj16@aol.com>
2021-03-07 20:12:56 -0700

    redid logic and fixed style

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# weather-app
+# Weather-App
+
+## Deployed Link
+
+[Deployed link]()
+
+## Description
+
+Awea
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Contributors](#contributors)
+* [Test](#test)
+* [Username](#username)
+* [Email](#email)
+* [Questions](#questions)
+    
+## Installation
+
+console.table, inquirer and mysql.
+
+## Contributors
+
+Cody Johnson
+
+## Test
+
+run node index.js in the command line at root folder.
+
+## Username
+
+codymjohnson
+
+## Email
+
+cmichael.johnson@outlook.com
+
+## Questions
+
+I can be reached at
+<br>
+Github link: https://github.com/codymjohnson 	 Email: cmichael.johnson@outlook.com
--- script.js
+++ script.js
@@ -1,48 +1,156 @@
 $(document).ready(function () {
-    // weatherdashboard api
-    var reqURL = "https://openweathermap.org/api";
-    var apiKey = "0c91526a0c3aba75152d1ad1c9382706"
+    //api for weatherDashboard to be used and variables
+    var requestURL = "https://openweathermap.org/api";
+    var apiKey = "0c91526a0c3aba75152d1ad1c9382706";
 
-    // local storage for cities already searched
-    var citiesViewed = JSON.parse(localStorage.getItem("viewedCity")) || [];
-    // loop iterating through stored cities
-    for (var i = 0; i < citiesViewed.length; i++) {
-        listRender(citiesViewed[i]);
+    //local storage of searched cities
+    var searchedCities = JSON.parse(localStorage.getItem("citySearched")) || [];
+    //for loop to displayed searched cities from local storage
+    for (var i = 0; i < searchedCities.length; i++) {
+        listMaker(searchedCities[i]);
     }
 
-    // appending stored city list to html
-    function listRender(city) {
-        var li = $("<li>").addclass("list-group-item list-group-item-action").text(city);
-        $("#cityList").append(li);
+    function listMaker(city) {
+        var li = $("<li>")
+            .addClass("list-group-item list-group-item-action")
+            .text(city);
+        $("#listCity").append(li);
     }
 
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";
-    // searchbutton logic
-    $("searchBtn").on("click", function () {
-        var inputCity = $("inputField").val();
+    var queryURL =
+        "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+    //Search Button Value and input
+    $("#searchButton").on("click", function () {
+        console.log("hey");
+        var cityInput = $("#inputField").val();
         $("#inputField").val("");
-        searchWeather(inputCity);
+        weatherSearch(cityInput);
     });
 
-    // list event handler
-    $("#cityList").on("click", "li", function () {
-        searchWeather($(this).text());
-        console.log($(list).text());
+    //EVENT HANDLER for list
+    $("#listCity").on("click", "li", function () {
+        weatherSearch($(this).text());
+        console.log($(this).text());
     });
-
-    // weather search  fir current city
-    function searchWeather(inputCity) {
+    //Function for Current Weather Search
+    function weatherSearch(cityInput) {
         $.ajax({
             type: "GET",
-            url: "api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + apiKey + "&units=imperial",
+            url:
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                cityInput +
+                "&appid=" +
+                apiKey +
+                "&units=imperial",
             dataType: "json",
         }).then(function (data) {
             console.log(data);
-            if (citiesViewed.indexOf(inputCity) === -1) {
-                citiesViewed.push(inputCity);
-                localStorage.setItem("")
+            if (searchedCities.indexOf(cityInput) === -1) {
+                searchedCities.push(cityInput);
+                localStorage.setItem("citySearched", JSON.stringify(searchedCities));
+                listMaker(cityInput);
             }
-        })
-    }
 
+            $(".curChoice").empty();
+            //Creating current choice card and variables to display city choice
+            var newCard = $("<div>").addClass("card");
+            var cardBody = $("<div>").addClass("card-body");
+            var searchedCity = $("<h3>").addClass("card-title").text(data.name);
+            var weatherDates = $("<h5>")
+                .addClass("card-title")
+                .text(new Date().toLocaleDateString());
+            var windSpeed = $("<p>")
+                .addClass("card-text")
+                .text("Wind Speed: " + data.wind.speed);
+            var cityTemp = $("<p>")
+                .addClass("card-text")
+                .text("Temperature: " + data.main.temp);
+            var cityHumidity = $("<p>")
+                .addClass("card-text")
+                .text("Humidity: " + data.main.humidity);
+            var iconImage = $("<img>").attr(
+                "src",
+                "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+            );
+            //appending data retrieved to card
+            searchedCity.append(weatherDates, iconImage);
+            cardBody.append(searchedCity, windSpeed, cityTemp, cityHumidity);
+            newCard.append(cardBody);
+            $(".curChoice").append(newCard);
+            foreCast(cityInput);
+            uvIndex(data.coord.lat, data.coord.lon);
+        });
+    }
+    //function for forecast
+    function foreCast(cityInput) {
+        $.ajax({
+            type: "GET",
+            url:
+                "https://api.openweathermap.org/data/2.5/forecast?q=" +
+                cityInput +
+                "&appid=" +
+                apiKey +
+                "&units=imperial",
+            dataType: "json",
+        }).then(function (data) {
+            console.log(data);
+            $("#fiveDayForecast")
+                .html('<h3 class="mt-3">5-Day Forecast:</h4>')
+                .append('<div class="row">');
+            //for loop searching for 3pm all across the board
+            for (var i = 0; i < data.list.length; i++) {
+                if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+                    //variables to append to 5 day forecast cards
+                    var weatherDisplay = $("<div>").addClass("col-md-2");
+                    var newCard = $("<div>").addClass("card bg-primary text-white");
+                    var cardBody = $("<div>").addClass("card-body p-2");
+                    var weatherDates = $("<h5>")
+                        .addClass("card-title")
+                        .text(new Date(data.list[i].dt_txt).toLocaleDateString());
+                    var iconImage = $("<img>").attr(
+                        "src",
+                        "http://openweathermap.org/img/w/" +
+                        data.list[i].weather[0].icon +
+                        ".png"
+                    );
+                    var cityTemp = $("<p>")
+                        .addClass("card-text")
+                        .text("Temperature: " + data.list[i].main.temp);
+                    var cityHumidity = $("<p>")
+                        .addClass("card-text")
+                        .text("Humidity: " + data.list[i].main.humidity);
+                    //appending data retrieved
+                    cardBody.append(weatherDates, iconImage, cityTemp, cityHumidity);
+                    newCard.append(cardBody);
+                    weatherDisplay.append(newCard);
+                    $("#fiveDayForecast .row").append(weatherDisplay);
+                }
+            }
+        });
+    }
+    //UV index Function
+    function uvIndex(lat, lon) {
+        $.ajax({
+            type: "GET",
+            url:
+                "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+                lat +
+                "&lon=" +
+                lon +
+                "&appid=" +
+                apiKey,
+            dataType: "json",
+        }).then(function (data) {
+            console.log(data);
+            var uvTag = $("<button>").addClass("btn btn-sm").text(data.value);
+            if (data.value < 3) {
+                uvTag.addClass("btn-success");
+            } else if (data.value < 7) {
+                uvTag.addClass("btn-warning");
+            } else {
+                uvTag.addClass("btn-danger");
+            }
+            $(".curChoice .card-body").append(uvTag);
+        });
+    }
 });
